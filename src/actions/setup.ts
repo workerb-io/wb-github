344432841e8649055f376bc4e700684ae753b33a
Manoj Singh Negi <justanothermanoj@gmail.com>
2020-08-27 15:33:15 +0530

    fixed error handling

--- src/actions/setup.ts
+++ src/actions/setup.ts
@@ -2,11 +2,10 @@ let accessToken = prompt("Please provide your accessToken")
 
 if (!accessToken) {
     notify("accessToken can't be empty", "error", 3000)
+} else {
+    setVar('github', [{
+        name: 'accessToken',
+        value: accessToken
+    }])
+    reIndex(["github"])
 }
-
-setVar('github', [{
-    name: 'accessToken',
-    value: accessToken
-}])
-
-reIndex(["github"])
