9b589d18686251352ec18435d080079e6ea35e4f
Manoj Singh Negi <justanothermanoj@gmail.com>
2020-08-27 15:12:41 +0530

    aaded error for empty accessToken

--- src/actions/setup.ts
+++ src/actions/setup.ts
@@ -1,6 +1,12 @@
 let accessToken = prompt("Please provide your accessToken")
+
+if (!accessToken) {
+    notify("accessToken can't be empty", "error", 3000)
+}
+
 setVar('github', [{
     name: 'accessToken',
     value: accessToken
 }])
-reIndex(["github","repo"])
+
+reIndex(["github"])
