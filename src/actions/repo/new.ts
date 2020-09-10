89b748df730918d68accd05e6214eb028cf200e9
Manoj Singh Negi <justanothermanoj@gmail.com>
2020-09-10 13:30:40 +0530

    added logs

--- src/actions/repo/new.ts
+++ src/actions/repo/new.ts
@@ -1,4 +1,7 @@
 open("https://github.com/")
+
+log("opening github", "info")
+
 click('/html/body/div[1]/header/div[6]/details/summary', { method: 'by_xpath'})
 click('New repository', { expectReload: true })
 click('#repository_name', { method: 'by_query_selector'})
@@ -7,12 +10,15 @@ const organisationName = args[1]
 let repoName: string | null = args[0]
 
 if (!repoName) {
+    log("repo name was not provided asking for repo name", "warning")
     repoName = prompt("provide a valid name for your new repository")
 }
 
 let repoNameAsString = repoName as string
 
 type(repoNameAsString, '#repository_name', {method: 'by_query_selector'})
+
+log("checking if organisation name is provided", "info")
 
 if (organisationName) {
     click('[aria-describedby="repository-owner-label"]', {
@@ -22,6 +28,8 @@ if (organisationName) {
         method: "by_query_selector"
     })
 }
+
+log("Creating the repository", "info")
 
 click('Create repository', {
     expectReload: true
