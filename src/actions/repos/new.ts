d3e5464b5e641b8619d44bb86e30f3366c7065d2
Saurabh Garg <sgarg@workerb.io>
2020-09-19 11:10:36 -0700

    Updated new repo code

--- src/actions/repos/new.ts
+++ src/actions/repos/new.ts
@@ -1,24 +1,21 @@
 open("https://github.com/")
 
-log("opening github", "info")
+const organisationName = args[1]
+let repoName: string | null = args[0]
+
+if (!repoName) {
+    repoName = prompt("Name for the new repository")
+}
 
 click('/html/body/div[1]/header/div[6]/details/summary', { method: 'by_xpath'})
 click('New repository', { expectReload: true })
 click('#repository_name', { method: 'by_query_selector'})
 
-const organisationName = args[1]
-let repoName: string | null = args[0]
-
-if (!repoName) {
-    log("repo name was not provided asking for repo name", "warning")
-    repoName = prompt("provide a valid name for your new repository")
-}
-
 let repoNameAsString = repoName as string
 
 type(repoNameAsString, '#repository_name', {method: 'by_query_selector'})
 
-log("checking if organisation name is provided", "info")
+log("Checking if the organisation name is provided", "info")
 
 if (organisationName) {
     click('[aria-describedby="repository-owner-label"]', {
