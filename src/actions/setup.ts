7d75e243f3642909b0b0b97429a96d7f356ad1d4
praveen-me <praveenkrsaini0@gmail.com>
2020-09-20 05:21:29 +0530

    Add script for setup

--- src/actions/setup.ts
+++ src/actions/setup.ts
@@ -1,12 +1,61 @@
-let accessToken = prompt("Please provide your access token")
+let tokenName = "workerb";
 
-if (!accessToken) {
-    notify("Access token can't be empty", "error", 3000)
+open("https://github.com/settings/tokens");
+
+const currentUrl = readURL();
+
+if (currentUrl.indexOf("login") === -1) {
+  const allTokenNames = readAll("span.token-description a");
+
+  let workerbToken = "";
+
+  for (const token of allTokenNames) {
+    if (token.toLowerCase().indexOf(tokenName) !== -1) {
+      workerbToken = token;
+    }
+  }
+
+  tokenName = workerbToken
+    ? workerbToken.trim().slice(-1) ===
+      parseInt(workerbToken.trim().slice(-1), 10).toString()
+      ? `${workerbToken.trim().slice(0, -1)}${
+          Number(workerbToken.trim().slice(-1)) + 1
+        }`
+      : `${workerbToken.trim()}1`
+    : tokenName;
+
+  click("Generate new token", {
+    method: "by_text",
+  });
+
+  type(tokenName, "#oauth_access_description", {
+    method: "by_query_selector",
+  });
+
+  click(".token-scope input", {
+    method: "by_query_selector",
+  });
+
+  click("Generate token", {
+    method: "by_text",
+  });
+
+  let newAuthToken = read("#new-oauth-token", {
+    method: "by_query_selector",
+  });
+
+  if (!newAuthToken) {
+    notify("Access token can't be empty", "error", 3000);
+  } else {
+    setVar("github", [
+      {
+        name: "accessToken",
+        value: newAuthToken,
+      },
+    ]);
+    notify("Access token added successfully", "success", 3000);
+    reIndex();
+  }
 } else {
-    setVar('github', [{
-        name: 'accessToken',
-        value: accessToken
-    }])
-    notify("Access token added successfully", "success", 3000)
-    reIndex()
+  notify("Please login into your github account first", "error", 3000);
 }
