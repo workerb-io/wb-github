fdbe8c983c15be4454915ee346eff7da63e97542
Saurabh Garg <sgarg@workerb.io>
2020-09-26 17:43:20 -0700

    more moving around of things

--- src/actions/repos/option/PRs/get_options.ts
+++ src/actions/repos/option/PRs/get_options.ts
@@ -4,7 +4,7 @@ import { accessToken } from '../../../../utils/constants'
 let pullsList: any[] = []
 
 if (options.repos) {
-    var repoResponse = httpGet(getUrl(`/repos/${options.repo.owner.login}/${options.repo.name}/pulls?per_page=20&_=${new Date().getTime()}`), {
+    var repoResponse = httpGet(getUrl(`/repos/${options.repos.owner.login}/${options.repos.name}/pulls?per_page=20&_=${new Date().getTime()}`), {
         Authorization: "token " + accessToken
     })
     
