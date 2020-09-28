7474da6eeae545dbbeebe0661011a12863aa1b07
Saurabh Garg <sgarg@workerb.io>
2020-09-28 09:08:59 -0700

    Add comment

--- src/actions/repos/option/issues/option/comment.ts
+++ src/actions/repos/option/issues/option/comment.ts
@@ -7,7 +7,7 @@ if (options.issues) {
 	if (comment) {
 		open(options.issues.html_url)
 		type(comment, '#new_comment_field', { method: 'by_query_selector' })
-		submit()
+		click('Comment')
 		notify('Comment added to the issue', 'success', 3000)
 		reIndex(['github', 'repos', options.repos.name, 'issues'])
 	}
