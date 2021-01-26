fac9cac61943ff97a2e23dd6881db60d40fa672c
Puneet Aggarwal <aggarwal.puneet222@gmail.com>
2021-01-26 23:46:01 +0530

    Revert "GitHub organizations"

---
+++ src/actions/repos/option/issues/option/comment.ts
@@ -0,0 +1,17 @@
+// @description Comment on the issue
+if (options.issues) {
+	let comment: string | null = args.join(' ')
+	if (!comment) {
+		comment = prompt('Enter the comment')
+	}
+
+	if (comment) {
+		open(options.issues.html_url)
+		type(comment, '#new_comment_field', { method: 'by_query_selector' })
+		click('Comment')
+		notify('Comment added to the issue', 'success', 3000)
+		reIndex(['github', 'repos', options.repos.name, 'issues'])
+	}
+} else {
+	notify('Issue not found', 'error', 3000)
+}
