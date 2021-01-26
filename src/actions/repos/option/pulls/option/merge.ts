f2a250922c2ae330790bd1e21b24cab991828245
Puneet Aggarwal <aggarwal.puneet222@gmail.com>
2021-01-26 23:46:01 +0530

    Revert "GitHub organizations"

---
+++ src/actions/repos/option/pulls/option/merge.ts
@@ -0,0 +1,11 @@
+// @description Merge the pull request
+if (options.pulls) {
+	open(options.pulls.html_url)
+	click('Merge pull request')
+	click('Confirm merge')
+	notify('Pull request merged', 'success', 3000)
+	reIndex(['github', 'repos', options.repos.name])
+	reIndex(['github', 'repos', options.repos.name, 'pulls'])
+} else {
+	notify('Pull request not found', 'error', 3000)
+}
