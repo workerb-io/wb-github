d624b4d79537631d921f4af1fe5fdaafa1b254f3
puneet222 <aggarwal.puneet222@gmail.com>
2021-01-27 09:59:05 +0530

    Revert "Merge pull request #2 from puneet222/revert-1-github-organizations"
    This reverts commit 01ab6faa8eb9a9eb4ef7ecab3ebd31b3bcbbe92b, reversing
    changes made to 425dde991030a48d834b096d2b1e80b2934221a9.

--- src/actions/repos/option/issues/option/close.ts
+++
@@ -1,9 +0,0 @@
-// @description Close the issue
-if (options.issues) {
-	open(options.issues.html_url)
-	click('Close issue')
-	notify('Issue closed', 'success', 3000)
-	reIndex(['github', 'repos', options.repos.name, 'issues'])
-} else {
-	notify('Issue not found', 'error', 3000)
-}
--- src/actions/repos/option/pulls/option/close.ts
+++
@@ -1,9 +0,0 @@
-// @description Close the pull request
-if (options.pulls) {
-	open(options.pulls.html_url)
-	click('Close pull request')
-	notify('Pull request closed', 'success', 3000)
-	reIndex(['github', 'repos', options.repos.name, 'pulls'])
-} else {
-	notify('Pull request not found', 'error', 3000)
-}
