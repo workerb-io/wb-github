4fb81c653d4075d159de1b65bb6f54aab7e8f42a
Md. Sabbir Alam <sabbiralam.cse@gmail.com>
2021-03-03 18:27:00 +0600

    Fixed 'show all workflow' script

--- src/actions/orgs/option/repos/option/workflows/option/run.ts
+++ src/actions/orgs/option/repos/option/workflows/option/run.ts
@@ -5,10 +5,15 @@ if (options.repos) {
 		const htmlUrl = options.workflows.html_url
 		const splittedUrl = htmlUrl.split('/')
 		const ymlFileName = splittedUrl[splittedUrl.length - 1]
+		// open the currently selected workflow.
 		open(`${options.repos.html_url}/actions/workflows/${ymlFileName}`)
+
+		// click on the run workflow dropdown
 		click('/html/body/div[4]/div/main/div[2]/div/div/div/div[2]/div[5]/div[2]/details/summary', {
 			method: 'by_xpath',
 		})
+
+		// click on the run workflow item
 		click('/html/body/div[4]/div/main/div[2]/div/div/div/div[2]/div[5]/div[2]/details/div/div/div/form[2]/button', {
 			method: 'by_xpath',
 		})
--- src/actions/orgs/option/repos/option/workflows/option/view.ts
+++ src/actions/orgs/option/repos/option/workflows/option/view.ts
@@ -1,6 +1,5 @@
 // @description View the workflow
 if (options.repos) {
-	log(options)
 	if (options.workflows) {
 		open(options.workflows.html_url)
 	}
