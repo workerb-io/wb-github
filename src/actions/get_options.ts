970d27d9d5d50c386fe5c24c14edf109014bbcaf
Puneet Aggarwal <aggarwal.puneet222@gmail.com>
2021-03-19 23:31:35 +0530

    Added logout action (#22)
    Co-authored-by: Puneet Aggarwal <puneet@Puneets-MacBook-Pro.local>

--- src/actions/get_options.ts
+++ src/actions/get_options.ts
@@ -4,7 +4,7 @@ import { accessToken } from '../utils/constants'
 const returnOptions = () => {
 	if (!accessToken) {
 		return JSON.stringify({
-			remove: ['search', 'orgs'],
+			remove: ['search', 'orgs', 'logout'],
 		})
 	} else {
 		return JSON.stringify({
